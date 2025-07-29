d3ac54452a5038673af13a8e9cba34b9764187a9
ghostyboyy97 <revghostmusic@gmail.com>
2025-07-29 14:18:33 -0400

    feat (state): setup main re-render based on activeList

--- js/app.js
+++ js/app.js
@@ -1,6 +1,58 @@
+let activeList = false;
+let allLists = [];
 
+let list = {
+    uuid: 0,
+    title: "",
+    listItems: [],
+}
+
+let listItem = {
+    uuid: 0,
+    title: "",
+    desc: "",
+    itemStatus: "to_do",
+}
 
 function main(){
 
-    console.log("main working")
+    console.log("main working");
+
+    activeList ? showList(activeList) : showAllLists();
+
+
+}
+
+
+function showList(activeList){
+
+}
+
+function showAllLists(){
+    emptyListCtr();
+    $("main").append(`
+            <h2>Select a list below, or create a new one to get started!</h2>
+            <div class="list_ctr">
+                <div class="list_entry">
+                    <div class="list_entry__icon">></div>
+                    <div class="list_entry__title">Placeholder list 1</div>
+                    <button class="list_entry__button">View list -></button>
+                </div>
+                <div class="list_entry">
+                    <div class="list_entry__icon">></div>
+                    <div class="list_entry__title">Placeholder list 2</div>
+                    <button class="list_entry__button">View list -></button>
+                </div>
+                <div class="list_entry">
+                    <div class="list_entry__icon">></div>
+                    <div class="list_entry__title">Placeholder list 3</div>
+                    <button class="list_entry__button">View list -></button>
+                </div>
+                <button class="list_entry add_list" data-js-add-list="">Make a new list</button>
+            </div>
+    `)
+}
+
+function emptyListCtr(){
+    $("main").empty();
 }
