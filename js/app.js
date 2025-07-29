1899968979ec8631dcd483636cae6f7a4ea84adf
ghostyboyy97 <revghostmusic@gmail.com>
2025-07-29 15:24:12 -0400

    feat (list): populate data on a list item

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ let list = {
 
 let listItem = {
     uuid: 0,
-    title: "",
+    title: "Untitled list",
     desc: "",
     itemStatus: "to_do",
 }
@@ -35,10 +35,33 @@ function main(){
         showList(activeList);
     });
 
-    bodyRef.on("keyup", "#listTitle", function(){
-        console.log($(this).val())
-        list.title = $(this).val()
+    bodyRef.on("click", "[data-js-add-list-item]", function(){
+        let newID = Date.now();
+        let newListItem = {
+            uuid: newID,
+            title: "",
+            desc: "",
+            status: "to_do",
+        }
+        list.listItems.push(newListItem);
+        makeListItemUI(newListItem);
+
     });
+
+    bodyRef.on("change", "#listTitle", function(){
+        console.log($(this).val())
+        list.title = $(this).val();
+    });
+
+    bodyRef.on("change", ".list_ctr__item select, .list_ctr__item input, .list_ctr__item textarea", function(){
+        let uuid = parseInt($(this).closest("[data-js-list-item]").data("js-list-item"));
+        let listAttr = $(this).data("js-change-prop");
+        console.log(uuid)
+        let listItem = findItemInList(list, uuid);
+        listItem[listAttr] = $(this).val();
+        findItemInList(list, uuid);
+    });
+
 
 
     // turn back on to enable dynamic rendering once templating is done
@@ -79,4 +102,28 @@ function showAllLists(){
 
 function emptyListCtr(){
     $("main").empty();
+}
+
+function makeListItemUI(item){
+    let newItemUI = $(`
+        <div class="list_ctr__item" data-js-list-item="${item.uuid}">
+            <select name="itemStatus" data-js-change-prop="status" id="itemStatus${item.uuid}">
+                <option value="to_do">To do</option>
+                <option value="in_progress">In progress</option>
+                <option value="on_hold">On hold</option>
+                <option value="completed">Completed!</option>
+            </select>
+            <input type="text" data-js-change-prop="title" id="itemTitle${item.uuid}" placeholder="new list item">
+            <button data-js-delete-list-item="${item.uuid}">delete item</button>
+            <textarea data-js-change-prop="desc" placeholder="description"></textarea>
+        </div>
+    `)
+    newItemUI.find(`option[value=${item.status}`).attr("selected", "true");
+    $(".list_ctr__items").append(newItemUI);
+}
+
+function findItemInList(list, id){
+    let targetItem = list.listItems.find((el)=>{return el.uuid === id});
+    console.log(targetItem)
+    return targetItem;
 }
