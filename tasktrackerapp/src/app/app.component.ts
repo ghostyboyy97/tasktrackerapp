7285ced08f770b3491e5bf3ef1ea7d92f7bb4a33
ghostyboyy97 <revghostmusic@gmail.com>
2025-07-30 08:16:04 -0400

    feat (angular refactor): dynamic load of allItems set up, ability to delete items from a list

--- tasktrackerapp/src/app/app.component.ts
+++ tasktrackerapp/src/app/app.component.ts
@@ -17,31 +17,31 @@ export class AppComponent {
   LOCAL_STORAGE_KEY = "__ng_tasktrackerapp_data__";
 
   // for loop - track parameter is used to keep the dom association, can pass in any parameter and then access all parts of the child easily
-  allItems = [
-    {
-      uuid: 0,
-      title: "Landing page",
-      desc: "Set up the user interface for choosing a list",
-      status: "in_progress",
-    },
-    {
-      uuid: 1,
-      title: "List page",
-      desc: "Set up the user interface for adding/modifying/removing items from an individual list",
-      status: "to_do",
-    },
-    {
-      uuid: 2,
-      title: "Saving and loading",
-      desc: "Autosave changes to list items, and load those lists at runtine",
-      status: "to_do",
-    },
-    {
-      uuid: 3,
-      title: "Styling",
-      desc: "Make it all look pretty!",
-      status: "on_hold",
-    },
+  allItems:Array<Listitem> = [
+    // {
+    //   uuid: 0,
+    //   title: "Landing page",
+    //   desc: "Set up the user interface for choosing a list",
+    //   status: "in_progress",
+    // },
+    // {
+    //   uuid: 1,
+    //   title: "List page",
+    //   desc: "Set up the user interface for adding/modifying/removing items from an individual list",
+    //   status: "to_do",
+    // },
+    // {
+    //   uuid: 2,
+    //   title: "Saving and loading",
+    //   desc: "Autosave changes to list items, and load those lists at runtine",
+    //   status: "to_do",
+    // },
+    // {
+    //   uuid: 3,
+    //   title: "Styling",
+    //   desc: "Make it all look pretty!",
+    //   status: "on_hold",
+    // },
   ];
 
   addItem(){
@@ -51,6 +51,7 @@ export class AppComponent {
       desc: "",
       status: "to_do",
     });
+    this.saveItems();
   }
 
   get items() {
@@ -59,11 +60,22 @@ export class AppComponent {
 
   remove(item: Listitem){
     this.allItems.splice(this.allItems.indexOf(item), 1);
+    this.saveItems();
   }
 
   saveItems(){
     console.log("save")
     localStorage.setItem(this.LOCAL_STORAGE_KEY, JSON.stringify(this.allItems));
+  }
+
+  ngOnInit(){
+    console.log("setup")
+    let itemsToLoad = localStorage.getItem(this.LOCAL_STORAGE_KEY);
+    console.log(itemsToLoad);
+    if (typeof itemsToLoad){
+      this.allItems = JSON.parse(itemsToLoad || "[]");
+
+    }
   }
 
 }
